bee39a8b24726d3661a9c8fbeb7bd36efca40d9c
Clyde1063 <clyde.apples@gmail.com>
2022-07-06 21:43:08 +0200

    Outputting dynamic data to show on the page(const)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,14 +2,18 @@ import React from "react";
 import './Expenseitem.css'
 
 const ExpenseItem = () => {
+   const expenseDate = new Date(2022, 7, 7);
+   const expenseTitle = 'Car Insurance'
+   const expenseAmount = '200'
+
   return (
   <div className="expense-item">
       <div>
-         Date
+         {expenseDate.toISOString()}
       </div>
          <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <p className="expense-item__price">$134</p>
+            <h2>{expenseTitle}</h2>
+            <p className="expense-item__price">R{expenseAmount}</p>
          </div>
   </div>);
 };
