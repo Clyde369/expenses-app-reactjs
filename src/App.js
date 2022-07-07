035c7f873a46cf5f26deb2fe8afb7c29b2275f65
Clyde1063 <clyde.apples@gmail.com>
2022-07-07 08:04:33 +0200

    added props to the page and made it dynamic

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
+  const expenses = [
+  { title: 'Car insurance', amount: 300, date: new Date(2020,7,7)},
+  { title: 'Toilet paper', amount: 300, date: new Date(2020,7,7)},
+  { title: 'Sugar', amount: 300, date: new Date(2020,7,7)}
+  ];
+    
+  
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>this is a paragraph</p>
-      <ExpenseItem/>
+      <ExpenseItem 
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem 
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem 
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,15 @@
 import React from "react";
 import './Expenseitem.css'
 
-const ExpenseItem = () => {
-   const expenseDate = new Date(2022, 7, 7);
-   const expenseTitle = 'Car Insurance'
-   const expenseAmount = '200'
-
+const ExpenseItem = (props) => {
   return (
   <div className="expense-item">
       <div>
-         {expenseDate.toISOString()}
+         {props.date.toISOString()}
       </div>
          <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <p className="expense-item__price">R{expenseAmount}</p>
+            <h2>{props.title}</h2>
+            <p className="expense-item__price">R{props.amount}</p>
          </div>
   </div>);
 };
