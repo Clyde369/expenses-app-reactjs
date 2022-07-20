230505c5a1a9095e06a6cb52a44e377183d99864
Clyde1063 <clyde.apples@gmail.com>
2022-07-20 14:21:41 +0200

    Added a filter by year component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,21 @@
-import React, {useState} from "react";
+import React from "react";
 import ExpenseDate from '../Expenses/ExpenseDate'
 import '../Expenses/Expenseitem.css'
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
    //this a special value to update state in my project
-   const [title, setTitle] = useState(props.title)
-
-   const clickHandler = () => {
-      setTitle('Updated')
-      console.log(title)
-   }
   return (
   <Card className="expense-item">
          <ExpenseDate 
             date={props.date}
          />
          <div className="expense-item__description">
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <p className="expense-item__price">R{props.amount}</p>
          </div>
-         <button onClick={clickHandler}>Change Title</button>
-  </Card>);
+  </Card>
+  );
 };
 
 export default ExpenseItem;
